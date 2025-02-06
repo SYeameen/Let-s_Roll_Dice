a1dbe4bdfaca478c4f27109ce2b4db2c809429ff
SYeameen <sajjadyeameen@gmail.com>
2025-02-06 20:18:31 +0600

    just removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red[500],
         appBar: AppBar(
